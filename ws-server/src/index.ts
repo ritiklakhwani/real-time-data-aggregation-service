f28f715bef8af3fb4c8836dc0c9eb9f774012771
ritiklakhwani <ritiklakhwani28@gmail.com>
2026-03-02 02:06:18 +0530

    feat: added redis connection for subscribing to pubsub and done with the websocket for real time data flow

--- ws-server/src/index.ts
+++ ws-server/src/index.ts
@@ -3,34 +3,39 @@ import { createClient } from "redis";
 import dotenv from "dotenv";
 dotenv.config();
 
-const PORT = process.env.PORT ? parseInt(process.env.PORT) : undefined;
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3002;
 
+//redis connection
 const rediSub = await createClient()
   .on("error", (err) => console.log("Redis Sub Error", err))
   .connect();
-const redisReader = await createClient()
-  .on("error", (err) => console.log("Redis Reader Error", err))
-  .connect();
-
 console.log("redis connected")
 
 const wss = new WebSocketServer({ port: PORT });
 console.log(`WebSocket server running on port ${PORT}`);
 
+const clients = new Set<WebSocket>();
+
+//websocket connection
 wss.on("connection", async (ws) => {
   console.log("client connected, total:", clients.size + 1);
   clients.add(ws);
 
-  const existing = await redisReader.get("tokens:solana:latest");
-  if (existing) {
-    ws.send(JSON.stringify({
-      type: "initial_data",
-      data: JSON.parse(existing)
-    }));
-  }
+  await rediSub.subscribe("token:updates", (message) => {
+    for(const client of clients){
+        if(client.readyState === WebSocket.OPEN){
+            client.send(JSON.stringify({
+                type: "price_updates",
+                data: JSON.parse(message)
+            }))
+        }
+    }
+})
 
   ws.on("close", () => {
     clients.delete(ws);
     console.log("client disconnected, total:", clients.size);
   });
 });
+
+console.log("subscribed to token:updates channel");
