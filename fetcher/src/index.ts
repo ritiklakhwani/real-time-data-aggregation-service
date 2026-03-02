20a3a4bdb391988aeb3b639fdae80b325682ebfa
ritiklakhwani <ritiklakhwani28@gmail.com>
2026-03-03 05:18:42 +0530

    fix: updated cron job to run every 500 miliseconds

--- fetcher/src/index.ts
+++ fetcher/src/index.ts
@@ -47,6 +47,6 @@ async function runFetchCycle() {
 
 runFetchCycle();
 
-cron.schedule("*/10 * * * * *", runFetchCycle);
+cron.schedule("*/1/2 * * * * *", runFetchCycle);
 
 console.log("fetcher started");
